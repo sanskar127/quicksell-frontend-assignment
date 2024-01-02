207a5ed53a8406b86e89badb83c5b24fdde431c9
Sanskar Pandey <sanskarpandey281@gmail.com>
2024-01-02 18:00:29 +0530

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BackendContext } from './context/BackendContext.js'
+import { BackendContext } from './context/BackendContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
